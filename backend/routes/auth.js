7c33b379c73e08924d17d01ad290445afe34fc5c
MaitriSoni17 <maitrisoni171@gmail.com>
2025-10-07 21:47:48 +0530

    API for getting Business Owner Details

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const BusinessOwner = require('../models/BusinessOwner');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fetchbusinessowner = require('../middleware/fetchbusinessowner');
 
 const { body, validationResult } = require('express-validator');
 
@@ -34,7 +35,8 @@ router.post('/createbusinessowner', [
             phone: req.body.phone,
             address: req.body.address
         })
-        const authToken = jwt.sign({ businessowner: { id: businessowner.id } }, JWT_SECRET);
+        data = { businessowner: { id: businessowner.id } };
+        const authToken = jwt.sign(data, JWT_SECRET);
         // console.log(authToken);
         res.json({authToken});
     }
@@ -45,7 +47,7 @@ router.post('/createbusinessowner', [
 });
 
 
-// Authenticate a Business Owner using: POST "/api/auth/login". No login required
+// Authenticate a Business Owner using: POST "/api/auth/loginbusinessowner". No login required
 
 router.post('/loginbusinessowner', [
     body('email', 'Enter a valid email').isEmail(),
@@ -65,8 +67,24 @@ router.post('/loginbusinessowner', [
         if (!passwordCompare) {
             return res.status(400).json({ error: "Please try to login with correct credentials" });
         }
-        const authToken = jwt.sign({ businessowner: { id: businessowner.id } }, JWT_SECRET);
+        const data = { businessowner: { id: businessowner.id } };
+        const authToken = jwt.sign(data , JWT_SECRET);
         res.json({ authToken });
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send("Internal Server error occurred");
+    }
+});
+
+// Get Business Owner Data using: POST "/api/auth/getbusinessowner". Login required
+
+router.post('/getbusinessowner', fetchbusinessowner, async (req, res) => {
+
+    try {
+        userId = req.businessowner.id;
+        const businessowner = await BusinessOwner.findById(userId).select("-password");
+        res.send(businessowner);
     }
     catch (err) {
         console.error(err.message);
