bcac00ce8576421bb3ed241443386cf9bef05f12
MaitriSoni17 <maitrisoni171@gmail.com>
2025-12-25 16:30:41 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Import enhanced responsive styles first
+import './components/styles/enhanced-responsive.css';
+import './App.css';
 import Home from './components/Home';
 import Features from './components/Features';
 import About from './components/About';
